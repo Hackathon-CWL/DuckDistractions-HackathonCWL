bbf8623b46c90b9e3cee928a87cdd4da9a46d82c
Daamin909 <ashai.daamin@gmail.com>
2024-08-10 14:28:34 +0530

    edited scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,35 +16,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     showSection('get-started');
-
-    document.querySelectorAll('.tab-button').forEach(button => {
-        button.addEventListener('click', function() {
-            const tabId = button.getAttribute('data-tab');
-            showTab(tabId);
-        });
-    });
-
-    // Function to show the selected tab
-    function showTab(tabId) {
-        const tabs = document.querySelectorAll('.tab-panel');
-        const buttons = document.querySelectorAll('.tab-button');
-        
-        // Hide all tab panels
-        tabs.forEach(tab => {
-            if (tab.id === tabId) {
-                tab.style.display = 'block';
-            } else {
-                tab.style.display = 'none';
-            }
-        });
-
-        // Update tab button styles
-        buttons.forEach(button => {
-            if (button.getAttribute('data-tab') === tabId) {
-                button.classList.add('active');
-            } else {
-                button.classList.remove('active');
-            }
-        });
-    }
 });

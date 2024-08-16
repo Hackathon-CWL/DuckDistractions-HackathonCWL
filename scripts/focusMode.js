58f60a370f32ac31ed72a5861fc597a3b9476948
Daamin909 <ashai.daamin@gmail.com>
2024-08-16 18:06:55 +0530

    removed bug

--- scripts/focusMode.js
+++ scripts/focusMode.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const focusModeContainer = document.getElementById('focus-mode-container');
     function toggleFocusModeButton() {
-        //if (currentSection === 'pomodoro' || currentSection === 'todo-list' || currentSection === 'analytics') {
             focusModeContainer.style.display = 'flex';
-       // } else {
-            //focusModeContainer.style.display = 'none'; 
-       // }
     }
     toggleFocusModeButton();
     const menuButtons = document.querySelectorAll('.menuButtons');
